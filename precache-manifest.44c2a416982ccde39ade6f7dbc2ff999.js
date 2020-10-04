6936f2fd5cc08b4537b8dd3206c02fffc1fbfd93
Juan Manuel Reisz <42250523+jreisz@users.noreply.github.com>
2020-10-04 00:12:58 -0300

    Updates

---
+++ precache-manifest.44c2a416982ccde39ade6f7dbc2ff999.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46a19fdf57544e4c826be3ec1506d26",
+    "url": "/TURAS-229-UI-Menu/index.html"
+  },
+  {
+    "revision": "36f7e57c2c536ba46511",
+    "url": "/TURAS-229-UI-Menu/static/css/main.10169b9e.chunk.css"
+  },
+  {
+    "revision": "79148b17851906e7dd85",
+    "url": "/TURAS-229-UI-Menu/static/js/2.d7d74003.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TURAS-229-UI-Menu/static/js/2.d7d74003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f7e57c2c536ba46511",
+    "url": "/TURAS-229-UI-Menu/static/js/main.fe4cdd2d.chunk.js"
+  },
+  {
+    "revision": "6214b71371ed6d4b9ac1",
+    "url": "/TURAS-229-UI-Menu/static/js/runtime-main.3fb0de33.js"
+  },
+  {
+    "revision": "3001cff3181eefd8db327cd724309a09",
+    "url": "/TURAS-229-UI-Menu/static/media/Caveat-Bold.3001cff3.ttf"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/TURAS-229-UI-Menu/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "de70a8a9ce5dcf6f9e5046ad0af9abac",
+    "url": "/TURAS-229-UI-Menu/static/media/Inter-Black-slnt=0.de70a8a9.ttf"
+  },
+  {
+    "revision": "c73899dff65a846e3d8423b04cb041ec",
+    "url": "/TURAS-229-UI-Menu/static/media/Inter-Bold-slnt=0.c73899df.ttf"
+  },
+  {
+    "revision": "1af0b153d5633ca641badc9e7dfe8245",
+    "url": "/TURAS-229-UI-Menu/static/media/RockSalt-Regular.1af0b153.ttf"
+  },
+  {
+    "revision": "f470424aceb2d633d330666b26b03466",
+    "url": "/TURAS-229-UI-Menu/static/media/background.f470424a.png"
+  },
+  {
+    "revision": "9f8d26fdb4863b17f6ea8d8acccd9ae9",
+    "url": "/TURAS-229-UI-Menu/static/media/background@2x.9f8d26fd.png"
+  },
+  {
+    "revision": "abcaf3c423fcc9ff13d67c648baf3ef9",
+    "url": "/TURAS-229-UI-Menu/static/media/background@3x.abcaf3c4.png"
+  },
+  {
+    "revision": "2c008f9a42f032b2262d2d6d33f03498",
+    "url": "/TURAS-229-UI-Menu/static/media/journey.2c008f9a.png"
+  }
+]);
